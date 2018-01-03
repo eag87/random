9bf52bbf0ae8434dbbbd7db351b11d3d157317ea
Enrico-Andrea <34809875+eag87@users.noreply.github.com>
2018-01-03 19:06:17 +0100

    Update function_random.swift

--- function_random.swift
+++ function_random.swift
@@ -4,6 +4,12 @@ func random(range:Int) -> Int {
 }
 
 //funzione che ha come parametro di ingresso un numero (il range) ed in quel range genera un numero casuale, partendo da 1 ... range
-func random(range:Int) -> Int {
+func random2(range:Int) -> Int {
     return Int(arc4random_uniform(UInt32(range))+1)
 }
+
+//utilizzo, per generare un numero da 0 a 5
+let risultato_dandom = random(range: 6)
+
+//utilizzo, per generare un numero da 1 a 6
+let risultato_dandom2 = random2(range: 6)
